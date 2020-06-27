63e1655ebfd5fc73323dae3a61e49bffd01084f3
shadow <shadow@JMA.local>
2020-06-26 23:08:29 -0500

    build folder included

---
+++ client/build/precache-manifest.1cc1f155743ae668312bc616ec91e6cb.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0d3d64dc81481890075ca85a6fe5b07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7e4e2d94f9977a4f12c",
+    "url": "/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "317db53c05629513f459",
+    "url": "/static/css/main.3797c533.chunk.css"
+  },
+  {
+    "revision": "c7e4e2d94f9977a4f12c",
+    "url": "/static/js/2.ca019651.chunk.js"
+  },
+  {
+    "revision": "c6c59d5f8ef7ccf621479fa060375eac",
+    "url": "/static/js/2.ca019651.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "317db53c05629513f459",
+    "url": "/static/js/main.c450d433.chunk.js"
+  },
+  {
+    "revision": "7269b0c4207b44d8a9e4f0b85c8a81d2",
+    "url": "/static/js/main.c450d433.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/static/media/sidebar-4.fc9cb053.jpg"
+  },
+  {
+    "revision": "0739f557949d21f40feaf1599b031814",
+    "url": "/static/media/sidebar.0739f557.jpg"
+  }
+]);
