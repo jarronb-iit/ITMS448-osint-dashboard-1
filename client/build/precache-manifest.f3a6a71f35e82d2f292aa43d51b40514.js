fb2abb36d7f369ba08d5ed81a1f08ba187ede444
Jarronb <baileyjarron@gmail.com>
2020-07-10 02:01:46 -0500

    update

---
+++ client/build/precache-manifest.f3a6a71f35e82d2f292aa43d51b40514.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f690d0039aab599c52c74f0de2fb21f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "840179a3b69b52ffc3a9",
+    "url": "/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "5e561c50cc9dc6803737",
+    "url": "/static/css/main.3797c533.chunk.css"
+  },
+  {
+    "revision": "840179a3b69b52ffc3a9",
+    "url": "/static/js/2.bffedd18.chunk.js"
+  },
+  {
+    "revision": "c6c59d5f8ef7ccf621479fa060375eac",
+    "url": "/static/js/2.bffedd18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e561c50cc9dc6803737",
+    "url": "/static/js/main.cc660bf2.chunk.js"
+  },
+  {
+    "revision": "7269b0c4207b44d8a9e4f0b85c8a81d2",
+    "url": "/static/js/main.cc660bf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/static/media/sidebar-4.fc9cb053.jpg"
+  },
+  {
+    "revision": "0739f557949d21f40feaf1599b031814",
+    "url": "/static/media/sidebar.0739f557.jpg"
+  }
+]);
